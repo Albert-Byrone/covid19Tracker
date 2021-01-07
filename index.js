5667858c11cd4a560a018cfea60012c7a34376aa
albertbyrone <albertbyrone16777@gmail.com>
2021-01-07 17:56:10 +0300

    imports

---
+++ index.js
@@ -0,0 +1,7 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const data = require('./router/data.js');
+const mongoose = require('mongoose');
+const model = require('./model/coviddata.js');
+var contactController = require('./controller/controller');
